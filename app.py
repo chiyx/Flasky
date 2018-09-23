cdc01f9345a3155be41b14addb4d90fffec15186
qianshang <qianshang@taobao.com>
2018-09-23 11:04:17 +0800

    ch03 jinja2

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_find(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
